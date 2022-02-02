a1a6dd26563090b2930a553bd499eac8d1165ced
kathirgit <k.kathir.sk@gmail.com>
2022-02-03 00:34:37 +0530

    Updating testsuites

--- src/test/java/testcases/BaseTest.java
+++ src/test/java/testcases/BaseTest.java
@@ -1,5 +1,24 @@
 package testcases;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
 public class BaseTest {
+
+	RemoteWebDriver driver = null;
+	WebElement element = null;
+
+	@Test
+	public void loadGmail() throws Exception {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\LENOVO\\eclipse-workspace\\AutomationProject\\src\\test\\resources\\Drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.gmail.com");
+		Thread.sleep(3000);
+		driver.close();
+		
+	}
 
 }
